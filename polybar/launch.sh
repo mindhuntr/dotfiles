839abe3f5113db6ad8e081fe8639cbc17e8df3cc
Irfan Jalal <mindhunter@blinkenshell.org>
2025-01-13 19:52:48 +0530

    added kitty and a few other changes

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,7 +2,7 @@
 
 killall -q polybar
 
-polybar --config=~/.config/polybar/config.ini complete 2>&1 | tee -a /tmp/polybar.log & disown
+polybar --config=~/.config/polybar/config.ini complete >/dev/null 2>&1 | tee -a /tmp/polybar.log & disown
 # polybar --config=~/.config/polybar/config.ini left 2>&1 &
 # polybar --config=~/.config/polybar/config.ini center 2>&1 &
 
