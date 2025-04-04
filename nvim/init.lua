1b87a46044cd7c851bd3f5b2c0bab784ac9cfdf2
Irfan Jalal <mindhunter@blinkenshell.org>
2025-04-04 21:03:44 +0530

    new set of changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -674,13 +674,12 @@ require('mason').setup()
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  -- clangd = {},
+  clangd = {},
   -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
+  pyright = {},
+  rust_analyzer = {},
+  bashls = {},
   -- tsserver = {},
-  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
-
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
