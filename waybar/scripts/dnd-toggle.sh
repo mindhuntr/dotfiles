25b2fcf7ae19900839d217b98473b96aa3c9f87c
Irfan Jalal <mindhunter@blinkenshell.org>
2025-09-01 11:26:07 +0530

    tweaks to hyprdots

--- waybar/scripts/dnd-toggle.sh
+++ waybar/scripts/dnd-toggle.sh
@@ -2,13 +2,19 @@
 
 
 toggle() { 
-    makoctl mode -t do-not-disturb
+    if [[ "$(makoctl mode)" == "default" ]]; then
+        makoctl mode -r default
+        makoctl mode -s do-not-disturb 
+    else
+        makoctl mode -r do-not-disturb
+        makoctl mode -s default
+    fi
 } 
 status() {
-    if [[ -n "$(makoctl mode)" ]]; then
-        echo '{"text": " ", "tooltip": "Do Not Disturb On", "class": "enabled"}'
-    else
+    if [[ "$(makoctl mode)" == "default" ]]; then
         echo '{"text": " ", "tooltip": "Do Not Disturb Off", "class": "disabled"}'
+    else
+        echo '{"text": " ", "tooltip": "Do Not Disturb On", "class": "enabled"}'
     fi
 }
 
